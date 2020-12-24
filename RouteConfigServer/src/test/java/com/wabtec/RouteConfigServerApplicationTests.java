39dc1d53123a10b7abba06bf51cefe1d9b1eeacc
gurubrahmam <31346038+gurubrahmam@users.noreply.github.com>
2020-12-24 13:45:44 +0530

    Add files via upload

---
+++ RouteConfigServer/src/test/java/com/wabtec/RouteConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wabtec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
